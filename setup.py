79a1d7ef47eaef2f4be13c18497eb35643bc4b1d
Aleksandr Shishkov <logis1936@gmail.com>
2023-11-30 13:21:47 +0200

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "LLM-Instruction-tuning-School-math-questions"
+AUTHOR_USER_NAME = "Logisx"
+SRC_REPO = "src"
+AUTHOR_EMAIL = "entbappy73@gmail.com"
+
+
+
 setup(
-    name='src',
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
     packages=find_packages(),
-    version='0.1.0',
-    description='A short description of the project.',
-    author='Aleksahishkov',
-    license='MIT',
+    license="MIT",
 )
+
