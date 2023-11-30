2a1d03d61f8fd2ce9087eba8e77b537643273c89
Aleksandr Shishkov <logis1936@gmail.com>
2023-11-30 13:32:52 +0200

    Added logging

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ __version__ = "0.0.0"
 REPO_NAME = "LLM-Instruction-tuning-School-math-questions"
 AUTHOR_USER_NAME = "Logisx"
 SRC_REPO = "src"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "logis1936@gmail.com"
 
 
 
